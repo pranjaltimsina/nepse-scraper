9196361d1d03f99d51ea42aaeefc8263544e97ca
Pranjal Timsina <pranjal.timsina@live.com>
2021-08-07 11:44:13 +0545

    feat: added multithreading

--- src/main.py
+++ src/main.py
@@ -4,11 +4,16 @@ import logging
 import sys
 import time
 
+from multiprocessing import Pool, cpu_count, Lock
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 from symbols import get_completed, get_symbols
 from get_max_page import get_max_page
+
+options = Options()
+options.headless = True
 
 def setup_logger(logger_name, log_file, level=logging.INFO):
     l = logging.getLogger(logger_name)
@@ -22,36 +27,13 @@ def setup_logger(logger_name, log_file, level=logging.INFO):
     l.addHandler(fileHandler)
     l.addHandler(streamHandler)
 
-def main(argv):
-  setup_logger('compact_log', './compact.log')
-  setup_logger('verbose_log', './verbose.log')
+def scrape(symbol):
+  setup_logger('compact_log', f'./compact_t{symbol[0]}.log')
+  setup_logger('verbose_log', f'./verbose_t{symbol[0]}.log')
   logger_c = logging.getLogger('compact_log')
   logger_v = logging.getLogger('verbose_log')
 
-  try:
-    symbols: list(str) = get_symbols()
-    logger_v.info('Symbols loaded.')
-  except FileNotFoundError:
-    logger_v.error("Symbols file not found.")
-    logger_c.error("Symbols file not found.")
-    sys.exit()
-
-  try:
-    completed = get_completed()
-    for complete in completed:
-      complete = complete.strip()
-      try:
-        symbols.remove(complete)
-      except ValueError:
-        logger_c.warning(f'Symbol {complete} not in the list of symbols')
-        logger_v.warning(f'Symbol {complete} not in the list of symbols')
-    logger_v.info('Scraped symbols list loaded.')
-  except FileNotFoundError:
-    logger_c.warning('Could not find file with symbols that have already been scraped.')
-    logger_v.warning('Could not find file with symbols that have already been scraped.')
-
-  options = Options()
-  options.headless = True
+  symbol = symbol[1]
 
   try:
     driver = webdriver.Chrome(executable_path=r'D://Dev/chromedriver.exe', options=options)
@@ -61,117 +43,142 @@ def main(argv):
     logger_c.info('Driver start up failed')
     sys.exit()
 
-  if (len(argv) == 1):
-    symbol = argv[0]
-    logger_v.info(f"{symbol} recieved from argv.")
-    if symbol.strip() not in symbols:
-      logger_c.error(f"{symbol} revieved not in list of symbols.")
-      logger_v.error(f"{symbol} revieved not in list of symbols.")
-      sys.exit()
-    else:
-      logger_v.info(f'{symbol} found in the list of symbols.')
-      symbols = [symbol]
-  else:
-    symbols = symbols[:5]
-    logger_v.info(f"No argv supplied, {symbols} are being scraped.")
+  start_time = time.time()
+  logger_c.info(f"{symbol} is being scraped.")
+  logger_v.info(f"{symbol} is being scraped.")
+  try:
+    driver.get(f"https://www.sharesansar.com/company/{symbol}")
+    logger_v.info('Successfully opened site')
+  except:
+    logger_c.error('Could not GET site.')
+    logger_v.error('Could not GET site.')
 
-  for symbol in symbols:
-    start_time = time.time()
-    logger_c.info(f"{symbol} is being scraped.")
-    logger_v.info(f"{symbol} is being scraped.")
-    try:
-      driver.get(f"https://www.sharesansar.com/company/{symbol}")
-      logger_v.info('Successfully opened site')
-    except:
-      logger_c.error('Could not GET site.')
-      logger_v.error('Could not GET site.')
-      continue
+  try:
+    price_button = driver.find_element_by_id('btn_cpricehistory')
+    price_button.click()
+    logger_v.info('Price Button  found and clicked on.')
+    time.sleep(3)
+  except:
+    logger_c.error('Price button couldn\'t be found and clicked on.')
+    logger_v.error('Price button couldn\'t be found and clicked on.')
+    driver.quit()
+    sys.exit()
 
-    try:
-      price_button = driver.find_element_by_id('btn_cpricehistory')
-      price_button.click()
-      logger_v.info('Price Button  found and clicked on.')
-      time.sleep(3)
-    except:
-      logger_c.error('Price button couldn\'t be found and clicked on.')
-      logger_v.error('Price button couldn\'t be found and clicked on.')
+  try:
+    table = driver.find_element_by_id("myTableCPriceHistory")
+    logger_v.info('Table found')
+  except:
       driver.quit()
-      sys.exit()
+      logger_v.error('Table not found. Driver Closed')
+      logger_c.error('Table not found. Driver Closed')
 
-    try:
-      table = driver.find_element_by_id("myTableCPriceHistory")
-      logger_v.info('Table found')
-    except:
+  header = table.text
+  header = ",".join(header.split(" "))
+  logger_v.info('Header text scraped')
+
+  buttons = driver.find_elements_by_class_name('paginate_button')
+  max_page = get_max_page(buttons)
+  logger_c.info(f'There are {max_page} pages for {symbol}')
+  logger_v.info(f'There are {max_page} pages for {symbol}')
+
+  with open (f'../data/price_history/{symbol}.csv', 'w') as file:
+    file.writelines([header])
+    file.write("\n")
+
+    current_page = 2
+    while 1:
+      logger_v.info(f'Current page is {current_page}')
+
+      try:
+        buttons = driver.find_elements_by_class_name('paginate_button')
+        logger_v.info('Found the navigation buttons.')
+      except:
+        logger_c.error('Couldn\'t find page naviagation buttons')
+        logger_v.error('Couldn\'t find page naviagation buttons')
         driver.quit()
-        logger_v.error('Table not found. Driver Closed')
-        logger_c.error('Table not found. Driver Closed')
+        sys.exit()
 
-    header = table.text
-    header = ",".join(header.split(" "))
-    logger_v.info('Header text scraped')
+      for button in buttons:
+        if button.text == 'Previous' or button.text == 'Next':
+          continue
 
-    buttons = driver.find_elements_by_class_name('paginate_button')
-    max_page = get_max_page(buttons)
-    logger_c.info(f'There are {max_page} pages for {symbol}')
-    logger_v.info(f'There are {max_page} pages for {symbol}')
+        if (int(button.text) == current_page):
+          button.click()
+          logger_v.info('Target button found and clicked.')
+          time.sleep(4)
+          try:
+            table = driver.find_element_by_id("myTableCPriceHistory")
+            table_body = table.find_element_by_tag_name("tbody")
+            logger_v.info('Table data found')
+          except:
+            logger_c.error('Table data not found.')
+            logger_v.error('Table data not found.')
+            driver.quit()
+            sys.exit()
 
-    with open (f'../data/price_history/{symbol}.csv', 'w') as file:
-      file.writelines([header])
-      file.write("\n")
+          body_text = table_body.text.replace("NO RECORD FOUND", r"\n").replace(',', "").replace(" ", ",").split(r"\n")
 
-      current_page = 2
-      while 1:
-        logger_v.info(f'Current page is {current_page}')
-
-        try:
-          buttons = driver.find_elements_by_class_name('paginate_button')
-          logger_v.info('Found the navigation buttons.')
-        except:
-          logger_c.error('Couldn\'t find page naviagation buttons')
-          logger_v.error('Couldn\'t find page naviagation buttons')
-          driver.quit()
-          sys.exit()
-
-        for button in buttons:
-          if button.text == 'Previous' or button.text == 'Next':
-            continue
-
-          if (int(button.text) == current_page):
-            button.click()
-            logger_v.info('Target button found and clicked.')
-            time.sleep(4)
-            try:
-              table = driver.find_element_by_id("myTableCPriceHistory")
-              table_body = table.find_element_by_tag_name("tbody")
-              logger_v.info('Table data found')
-            except:
-              logger_c.error('Table data not found.')
-              logger_v.error('Table data not found.')
-              driver.quit()
-              sys.exit()
-
-            body_text = table_body.text.replace("NO RECORD FOUND", r"\n").replace(',', "").replace(" ", ",").split(r"\n")
-
-            file.writelines(body_text)
-            file.write("\n")
-            logger_v.info(f'Successfully wrote page {current_page}')
-            break # Stop looking at other buttons
+          file.writelines(body_text)
+          file.write("\n")
+          logger_v.info(f'Successfully wrote page {current_page}')
+          break # Stop looking at other buttons
+      else:
+        current_page = current_page-1
+        if (current_page == max_page):
+          logger_c.info(f'{max_page}/{max_page} pages of {symbol} have been scraped successfull in {time.time() - start_time:.2f} seconds.')
+          logger_v.info(f'{max_page}/{max_page} pages of {symbol} have been scraped successfull in {time.time() - start_time:.2f} seconds.')
         else:
-          current_page = current_page-1
-          if (current_page == max_page):
-            logger_c.info(f'{max_page}/{max_page} pages of {symbol}have been scraped successfull in {time.time() - start_time:.2f} seconds.')
-            logger_v.info(f'{max_page}/{max_page} pages of {symbol}have been scraped successfull in {time.time() - start_time:.2f} seconds.')
-          else:
-            logger_v.error(f'Could not scrape all pages, stopped at {current_page} after {time.time() - start_time:.2f} seconds')
-            logger_v.error(f'Could not scrape all pages, stopped at {current_page} after {time.time() - start_time:.2f} seconds')
-          break # If all buttons have been looked at, then stop looking
-        current_page+=1
-      with open('../data/completed.txt', 'a') as outfile:
-        outfile.writelines(f"{symbol.strip().lower()}\n")
+          logger_v.error(f'Could not scrape all pages, stopped at {current_page}/{max_page} after {time.time() - start_time:.2f} seconds')
+          logger_v.error(f'Could not scrape all pages, stopped at {current_page}/{max_page} after {time.time() - start_time:.2f} seconds')
+        break # If all buttons have been looked at, then stop looking
+      current_page+=1
+    with open('../data/completed.txt', 'a') as outfile:
+      outfile.writelines(f"{symbol.strip().lower()}\n")
 
   driver.quit()
   logger_v.info('Closed driver.')
   logger_c.info('Closed driver.')
 
+def init_child(lock_):
+  global lock
+  lock = lock_
+
+def main():
+  setup_logger('main_log', './main.log')
+  main_logger = logging.getLogger('main_log')
+
+  try:
+    symbols: list(str) = get_symbols()
+    main_logger.info('Symbols loaded.')
+  except FileNotFoundError:
+    main_logger.error("Symbols file not found.")
+    sys.exit()
+
+  try:
+    completed = get_completed()
+    for complete in completed:
+      complete = complete.strip()
+      try:
+        symbols.remove(complete)
+      except ValueError:
+        main_logger.warning(f'Symbol {complete} not in the list of symbols')
+    main_logger.info('Scraped symbols list loaded.')
+  except FileNotFoundError:
+    main_logger.warning('Could not find file with symbols that have already been scraped.')
+
+  lock = Lock()
+  pool_size = cpu_count()
+
+  symbols = symbols[:pool_size]
+
+  main_logger.info(f"{symbols} will be scraped.")
+
+  new_symbols = []
+  for i, symbol in enumerate(symbols):
+    new_symbols.append((i, symbol))
+
+  with Pool(pool_size, initializer=init_child, initargs=(lock,)) as pool:
+    pool.map(scrape, new_symbols)
+
 if __name__ == '__main__':
-  main(sys.argv[1:])
+  main()
