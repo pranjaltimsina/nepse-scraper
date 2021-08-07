99b972d880da35eba6b506c3bba9c6a141e5a14e
Pranjal Timsina <pranjal.timsina@live.com>
2021-08-07 14:17:00 +0545

    fix: bug that caused crash if max-page couldnt be found

--- src/get_max_page.py
+++ src/get_max_page.py
@@ -1,11 +1,14 @@
 def get_max_page(buttons):
-  max_v = 1
-  for button in buttons:
-    value = button.text
-    try:
-      value = int(value)
-    except:
-      continue
-    if (value > max_v):
-      max_v = value
-  return max_v
+  try:
+    max_v = 1
+    for button in buttons:
+      value = button.text
+      try:
+        value = int(value)
+      except:
+        continue
+      if (value > max_v):
+        max_v = value
+    return max_v
+  except:
+    return 0
